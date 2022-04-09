ec8cb69cba793868637af3cbc0648dfe10b8010d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-09 13:34:17 +0800

    Delete re.py

--- re.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# example using re.compile
-
-import re
-
-phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
-mo = phoneNumRegex.search('My number is 206-555-1212')
-print mo.groups()
