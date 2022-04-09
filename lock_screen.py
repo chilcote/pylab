ccc7569a3ef7d8261602ffe686c6a69ca9329f1e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-09 13:11:26 +0800

    Delete lock_screen.py

--- lock_screen.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-## ref: https://gist.github.com/pudquick/350ba6411df3be77d32a
-
-from ctypes import CDLL
-
-loginPF = CDLL('/System/Library/PrivateFrameworks/login.framework/Versions/Current/login')
-result = loginPF.SACLockScreenImmediate()
